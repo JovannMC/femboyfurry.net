dd5ef212460768acf51bae3fbf01a9c17268cb77
JovannMC <45893380+JovannMC@users.noreply.github.com>
2023-04-14 19:11:31 +0300

    Fix borders when actively changing zoom/res
    Border disappears on navbar when opening burger menu (zoomed in) and zooming back out

--- static/js/navbar-burger.js
+++ static/js/navbar-burger.js
@@ -10,11 +10,40 @@ document.addEventListener('DOMContentLoaded', () => {
       const target = el.dataset.target;
       const $target = document.getElementById(target);
 
+      // Define the threshold width at which the hamburger menu appears
+      const hamburgerThresholdWidth = 1024;
+
       // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
       el.classList.toggle('is-active');
       $target.classList.toggle('is-active');
 
+      // Check if the viewport width is below the threshold for the hamburger menu
+      if (window.innerWidth < hamburgerThresholdWidth) {
+        const $navbarItems = Array.prototype.slice.call($target.querySelectorAll('.navbar-item'), 0);
+        $navbarItems.forEach((item) => {
+          item.classList.toggle('no-border');
+        });
+      }
     });
   });
 
+  // Add a resize event to check if the viewport width is below the threshold for the hamburger menu
+  window.addEventListener('resize', () => {
+    if (window.innerWidth >= hamburgerThresholdWidth) {
+      const $navbarItems = Array.prototype.slice.call(document.querySelectorAll('.navbar-item'), 0);
+      $navbarItems.forEach((item) => {
+        item.classList.remove('no-border');
+      });
+    }
+  });
+
+  // Add a scroll event to check if the viewport width is below the threshold for the hamburger menu
+  window.addEventListener('scroll', () => {
+    if (window.innerWidth >= hamburgerThresholdWidth) {
+      const $navbarItems = Array.prototype.slice.call(document.querySelectorAll('.navbar-item'), 0);
+      $navbarItems.forEach((item) => {
+        item.classList.remove('no-border');
+      });
+    }
+  });
 });
