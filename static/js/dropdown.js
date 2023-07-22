2e961f88d8a5ffe2cb92e4a31073dfa47d935263
JovannMC <45893380+JovannMC@users.noreply.github.com>
2023-07-22 22:06:22 +0800

    Add categories and start website pages
    Also remove </br> to use <br>

---
+++ static/js/dropdown.js
@@ -0,0 +1,20 @@
+ // Get all project categories with their respective projects
+ const projectCategories = document.querySelectorAll('.box .category');
+
+ // Add an event listener to each category to toggle its projects on click
+ projectCategories.forEach((category) => {
+   const categoryTitle = category.querySelector('.title');
+   const categoryProjects = category.querySelector('.column');
+   const dropdownArrow = categoryTitle.querySelector('.dropdown-arrow');
+
+   category.addEventListener('click', () => {
+     // Toggle visibility of the category's projects
+     if (categoryProjects.classList.contains('project-hidden')) {
+       categoryProjects.classList.remove('project-hidden');
+       dropdownArrow.classList.add('expanded');
+     } else {
+       categoryProjects.classList.add('project-hidden');
+       dropdownArrow.classList.remove('expanded');
+     }
+   });
+ });
