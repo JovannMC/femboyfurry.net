46365626de34d4a93611663b72532e1e65bfc03e
JovannMC <45893380+JovannMC@users.noreply.github.com>
2023-07-22 22:17:32 +0800

    Projects page: No JS compatibility
    Change behaviour so that the projects page shows all projects when there is no JS

--- static/js/dropdown.js
+++ static/js/dropdown.js
@@ -1,20 +1,31 @@
-// Get all project categories with their respective projects
-const projectCategories = document.querySelectorAll(".box .category");
+// Function to hide categories on page load
+function hideCategoriesOnLoad() {
+  const categoryProjects = document.querySelectorAll(".box .category .column");
+
+  categoryProjects.forEach((projects) => {
+    projects.style.display = "none"; // Hide the category projects on page load
+  });
+}
+
+// Call the function to hide categories when the site loads
+window.addEventListener("load", hideCategoriesOnLoad);
 
 // Add an event listener to each category to toggle its projects on click
-projectCategories.forEach((category) => {
-	const categoryTitle = category.querySelector(".title");
-	const categoryProjects = category.querySelector(".column");
-	const dropdownArrow = categoryTitle.querySelector(".dropdown-arrow");
+const projectCategories = document.querySelectorAll(".box .category");
 
-	category.addEventListener("click", () => {
-		// Toggle visibility of the category's projects
-		if (categoryProjects.classList.contains("project-hidden")) {
-			categoryProjects.classList.remove("project-hidden");
-			dropdownArrow.classList.add("expanded");
-		} else {
-			categoryProjects.classList.add("project-hidden");
-			dropdownArrow.classList.remove("expanded");
-		}
-	});
+projectCategories.forEach((category) => {
+  const categoryTitle = category.querySelector(".title");
+  const categoryProjects = category.querySelector(".column");
+  const dropdownArrow = categoryTitle.querySelector(".dropdown-arrow");
+
+  category.addEventListener("click", () => {
+    // Toggle visibility of the category's projects
+    if (categoryProjects.style.display === "none") {
+      categoryProjects.style.display = ""; // Or whatever display style you want to use
+      dropdownArrow.classList.add("expanded");
+    } else {
+      categoryProjects.style.display = "none";
+      dropdownArrow.classList.remove("expanded");
+    }
+  });
 });
